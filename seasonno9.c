d9291d46c2e4e3ed7406a13fabb84c507f06f8c4
6704062617369 <s6704062617369@email.kmutnb.ac.th>
2025-12-29 09:16:32 +0700

    Add season identification based on month input

---
+++ seasonno9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+	int ss;
+	
+	scanf("%d",&ss);
+	
+	switch(ss) {
+		case 1:
+			printf("winter"); break;
+		case 2:
+			printf("winter"); break;
+		case 3:
+			printf("summer"); break;
+		case 4:
+			printf("summer"); break;
+		case 5:
+			printf("summer"); break;
+		case 6:
+			printf("rainy"); break;
+		case 7:
+			printf("rainy"); break;
+		case 8:
+			printf("rainy"); break;
+		case 9:
+			printf("rainy"); break;
+		case 10:
+			printf("rainy"); break;
+		case 11:
+			printf("winter"); break;
+		case 12:
+			printf("winter"); break;
+		default:
+			printf("Invalid");
+	
+	return 0;
+	}
+}
